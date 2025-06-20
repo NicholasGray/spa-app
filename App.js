0977d368db2b56f3c3f390b25b29234be7c88e13
Nicholas Gray <nicholas_gray@live.com>
2025-06-20 17:11:13 -0400

    Fix blank screen by wrapping app in GestureHandlerRootView

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import HomeScreen from './src/screens/HomeScreen';
 import ChatScreen from './src/screens/ChatScreen';
@@ -12,13 +13,15 @@ const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Student Chat" component={ChatScreen} />
-        <Drawer.Screen name="Broadcasts" component={BroadcastsScreen} />
-        <Drawer.Screen name="Payments" component={PaymentsScreen} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Drawer.Navigator initialRouteName="Home">
+          <Drawer.Screen name="Home" component={HomeScreen} />
+          <Drawer.Screen name="Student Chat" component={ChatScreen} />
+          <Drawer.Screen name="Broadcasts" component={BroadcastsScreen} />
+          <Drawer.Screen name="Payments" component={PaymentsScreen} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
